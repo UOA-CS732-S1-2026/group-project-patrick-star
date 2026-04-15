32e7faf895cb4faf5bf71c04ea5f27e057209297
Jeraych <Jacky2022332467@gmail.com>
2026-04-15 18:07:21 +1200

    Created tests for user, find and fixed a bug on object id

--- backend/db/userService.js
+++ backend/db/userService.js
@@ -1,7 +1,7 @@
 const User = require("../models/User");
 
 const addUser = (data) => User.create(data);
-const getUser = (id) => User.findById({ id });
+const getUser = (id) => User.findById(id);
 const getUserByEmail = (email) => User.findOne({ email });
 const updateUser = (id, data) =>
   User.findByIdAndUpdate(id, data, { new: true });
