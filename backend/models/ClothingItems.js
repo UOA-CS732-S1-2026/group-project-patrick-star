cf0002ebdb95799f926eb9a1a5fe1cb29a466d3b
mzbzy10 <tzha578@aucklanduni.ac.nz>
2026-04-17 08:48:47 +1200

    added requirement for User.js and clothingItem.js models so the database will validate user inputs. Also made some fields compulsory

--- backend/models/ClothingItems.js
+++ backend/models/ClothingItems.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 
 const ClothingItemSchema = new mongoose.Schema({
-  userId: mongoose.Schema.Types.ObjectId,
-  name: String,
-  category: String,
-  size: String,
-  colour: String,
-  fit: String,
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+  
+  name: { type: String, required: true,  minlegth: 1, maxlength: 30},
+  category: { type: String, required: true, enum: ['top', 'bottom', 'outerwear', 'footwear', 'accessory'] },
+  size: { type: String, required: true, enum: ['XS', 'S', 'M', 'L', 'XL', 'XXL'] },
+  colour: { type: String, required: true },
+  fit: { type: String, required: true, enum: ['tight', 'regular', 'loose'] },
   imageUrls: {
     front: String,
     back: String,
