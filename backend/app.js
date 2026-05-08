aed21f8fd47c043228b9a69f5615ac42e3e94805
Jeraych <Jacky2022332467@gmail.com>
2026-05-08 17:54:53 +1200

    feat(outfit): Added outfits endpoints

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,14 @@ const app = express();
 const ClothingItemsRoutes = require("./routes/clothingItems");
 const UsersRoutes = require("./routes/users");
 const TryOnRoutes = require("./routes/tryon");
+const OutfitsRoutes = require("./routes/outfits");
 
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use("/api/clothingItems", ClothingItemsRoutes);
 app.use("/api/users", UsersRoutes);
 app.use("/api/tryon", TryOnRoutes);
+app.use("/api/outfits", OutfitsRoutes);
 
 // MongoDB Connection
 mongoose
